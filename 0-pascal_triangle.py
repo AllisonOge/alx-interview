fd1d2b24b82e69012f08b5baca13b4080250f2ce
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-05-30 23:28:22 +0000

    task: pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module that creates the function pascal_triangle(n) that returns a list
+of list of integers representing the Pascal triangle of n
+"""
+
+import math
+
+def pascal_triangle(n):
+    """
+    computes the pascal triangle
+    Parameters
+    ----------
+    n: int - size of pascal triangle
+
+    Returns
+    -------
+    p_triangle: List of List of int
+    """
+    # if n = 1
+    # [[1], [1, 1]]
+    # if n = 2
+    # [[1], [1, 1], [1, 2, 1]]
+    # if n = 3
+    # [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
+    # if n = 4
+    # [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    # if n = k
+    # [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1],...,[kC0, kC1, kC3, ..., kCk]]
+    # where kCr = k!/(k-r)!r!
+
+    if n <= 0:
+        return [[1],]
+
+    row = [math.factorial(n)/(math.factorial(n-i)*math.factorial(i)) for i in range(n + 1)]
+    return pascal_triangle(n-1) + [row]
