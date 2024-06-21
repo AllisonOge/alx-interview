f74bafa0cc7ff05bbe42eabaed60700831ad711b
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2024-06-21 01:29:31 +0000

    fixed code to meet challenge requirement

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,53 @@
 #!/usr/bin/python3
 """l0-stats.py"""
 
+import sys
+import signal
 
-def main(cmdline, store={"size": 0, "count": 0}):
-    """process args in command line and update store"""
-    # process args in command line
-    ip, cmdline_ = cmdline.split("-", 1)
-    cmdline = cmdline_.strip()
-    date, cmdline_ = cmdline.split("]", 1)
-    date = date.replace("[", "")
-    cmdline = cmdline_.strip()
-    url, cmdline_ = cmdline.split("\"", 2)[1:]
-    cmdline = cmdline_.strip()
-    status_code, file_size = list(map(int, cmdline.split()))
+store = {"size": 0, "count": 0, "status_codes": {}}
+valid_status_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
 
-    # update store
-    new_file_size = store.get("size") + file_size
-    new_count = store.get("count") + 1
-    new_status_count = store.get(str(status_code), 0) + 1
-    store.update({"size": new_file_size,
-                 "count": new_count,
-                  str(status_code): new_status_count})
+
+# Define a function to handle keyboard interruption
+def signal_handler(sig, frame):
+    """keyboard interupt signal handler"""
+    print_stats()
+    sys.exit(0)
+
+
+def print_stats():
+    """Print the accumulated statistics"""
+    print(f"File size: {store['size']}")
+    for code in sorted(store['status_codes'].keys()):
+        print(f"{code}: {store['status_codes'][code]}")
+
+
+def main():
+    """main"""
+    signal.signal(signal.SIGINT, signal_handler)
+
+    try:
+        for cmdline in sys.stdin:
+            ip, cmdline_ = cmdline.split("-", 1)
+            cmdline = cmdline_.strip()
+            date, cmdline_ = cmdline.split("]", 1)
+            date = date.replace("[", "")
+            cmdline = cmdline_.strip()
+            url, cmdline_ = cmdline.split("\"", 2)[1:]
+            cmdline = cmdline_.strip()
+            status_code, file_size = list(map(int, cmdline.split()))
+
+            store["size"] += int(file_size)
+            if status_code not in store["status_codes"]:
+                store["status_codes"][status_code] = 0
+            store["status_codes"][status_code] += 1
+            store["count"] += 1
+
+            if store["count"] % 10 == 0:
+                print_stats()
+    except KeyboardInterrupt:
+        print_stats()
 
 
 if __name__ == "__main__":
-    store = {"size": 0, "count": 0}
-    cmdline = input()
-    try:
-        while cmdline:
-            main(cmdline, store)
-
-            if store.get("count") % 10 == 0:
-                size = store.get("size", 0)
-                count = store.get("count", 0)
-                print(f"File size: {size}")
-                for k, v in sorted(store.items()):
-                    if k not in ["size", "count"]:
-                        print(f"{k}: {v}")
-                # reset store
-                store = {"size": size, "count": count}
-
-            cmdline = input()
-    except KeyboardInterrupt as e:
-        pass
+    main()
